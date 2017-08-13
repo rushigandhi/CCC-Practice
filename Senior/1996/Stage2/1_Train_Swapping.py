efe607e4331c4f79ac0c98d16828061048d4f3fb
rushigandhi <rushi.gandhi@hotmail.com>
2017-08-12 23:43:44 -0400

    (wip): 1996 Stage 2 Problem 1

---
+++ Senior/1996/Stage2/1_Train_Swapping.py
@@ -0,0 +1,24 @@
+total = int(input())
+for i in range(total):
+    listNum = int(input())
+    numList = [int(e) for e in input().split()]
+    print(numList)
+    counter = 0
+    for x in range(listNum - 1):
+        if numList[x + 1] < numList[x]:
+            tempData = numList[x + 1]
+            numList[x + 1] = numList[x]
+            numList[x] = tempData
+            counter += 1
+    print("Optimal train swapping takes", counter, "swap(s).")
+
+'''
+3
+3
+1 3 2
+4
+4 3 2 1
+2
+2 1
+
+'''
