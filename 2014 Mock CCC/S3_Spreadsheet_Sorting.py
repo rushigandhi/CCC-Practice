d0814885fc0faef451c706eac368341cb70eebf3
rushigandhi <rushi.gandhi@hotmail.com>
2017-08-14 15:55:06 -0400

    (wip): 2014 Mock CCC S3

---
+++ 2014 Mock CCC/S3_Spreadsheet_Sorting.py
@@ -0,0 +1,18 @@
+dimen = [int(e) for e in input().split()]
+#print(dimen)
+grid = list()
+for x in range(dimen[0]):
+    grid.append([int(e) for e in input().split()])
+numOfSorts = int(input())
+
+for x in range(numOfSorts):
+    colSort = int(input()) - 1
+    for i in range(len(grid) - 1):
+        if grid[i][colSort] > grid[i + 1][colSort]:
+            tempData = grid[i + 1]
+            grid[i + 1] = grid[i]
+            grid[i] = tempData
+
+for y in range(len(grid)):
+    line = ' '.join(str(e) for e in grid[y])
+    print(line)
