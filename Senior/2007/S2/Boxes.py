2cad0a1e2a8cce361fd6d28da9dec49c4a5971a9
rushigandhi <rushi.gandhi@hotmail.com>
2017-09-08 23:53:07 -0400

    Completed S2 2007 50/50 Score

--- Senior/2007/S2/Boxes.py
+++ Senior/2007/S2/Boxes.py
@@ -8,14 +8,16 @@ boxVol = sorted(boxVol)
 totalItems = int(input())
 for x in range(totalItems):
     dimensions = sorted([int(e) for e in input().split()])
+    outputList = list()
     for i in range(len(boxVol)):
         boxDimen = sorted(boxVol[i])
         if dimensions[0] <= boxDimen[0] and dimensions[1] <= boxDimen[1] and dimensions[2] <= boxDimen[2]:
-            print(boxDimen[0]*boxDimen[1]*boxDimen[2])
-            checker = False
-            break
-    if (dimensions[0] > boxDimen[0]) or (dimensions[1] > boxDimen[1]) or (dimensions[2] > boxDimen[2]):
+            outputList.append(boxDimen[0]*boxDimen[1]*boxDimen[2])
+    outputList = sorted(outputList)
+    if len(outputList) == 0:
         print("Item does not fit.")
+    else:
+        print(outputList[0])
 
 
 
