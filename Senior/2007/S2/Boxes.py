0929b10248bd7fae1895648fa9876173fb181608
rushigandhi <rushi.gandhi@hotmail.com>
2017-09-08 23:42:47 -0400

    (wip): 2007 S2 Doesn't Work for all Solutions

--- Senior/2007/S2/Boxes.py
+++ Senior/2007/S2/Boxes.py
@@ -7,11 +7,9 @@ for x in range(totalBoxes):
 boxVol = sorted(boxVol)
 totalItems = int(input())
 for x in range(totalItems):
-    dimensions = [int(e) for e in input().split()]
-    boxDimen = sorted(boxVol[len(boxVol) - 1])
+    dimensions = sorted([int(e) for e in input().split()])
     for i in range(len(boxVol)):
         boxDimen = sorted(boxVol[i])
-        dimensions = sorted(dimensions)
         if dimensions[0] <= boxDimen[0] and dimensions[1] <= boxDimen[1] and dimensions[2] <= boxDimen[2]:
             print(boxDimen[0]*boxDimen[1]*boxDimen[2])
             checker = False
