ec973d5e951f01ce0be90868cf49d24567852a9c
rushigandhi <rushi.gandhi@hotmail.com>
2017-09-09 23:51:48 -0400

    (wip): Nested Loop Approach

--- 2014 Mock CCC/S3_Spreadsheet_Sorting.py
+++ 2014 Mock CCC/S3_Spreadsheet_Sorting.py
@@ -3,18 +3,22 @@ dimen = [int(e) for e in input().split()]
 grid = list()
 for x in range(dimen[0]):
     grid.append([int(e) for e in input().split()])
-numOfSorts = int(input())
 
+numOfSorts = int(input())
 for x in range(numOfSorts):
     colSort = int(input()) - 1
-    for i in range(len(grid) - 1):
-        if grid[i][colSort] > grid[i + 1][colSort]:
-            tempData = grid[i + 1]
-            grid[i + 1] = grid[i]
-            grid[i] = tempData
-    line = ' '.join(str(e) for e in grid[i])
-    print(line)
+    for a in range(len(grid)):
+        print(grid)
+        for i in range(len(grid) - 1):
+            if grid[colSort][i] > grid[i + 1][colSort]:
+                tempData = grid[i + 1]
+                grid[i + 1] = grid[i]
+                grid[i] = tempData
+        # line = ' '.join(str(e) for e in grid[i])
+
 
 '''for y in range(len(grid)):
     line = ' '.join(str(e) for e in grid[y])
-    print(line)'''
+    print(line)
+  
+'''
