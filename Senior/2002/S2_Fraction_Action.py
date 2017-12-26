30c6c7cebb779a4a00e5a3991df3e3c20b9c04a7
rushigandhi <rushi.gandhi@hotmail.com>
2017-12-25 23:52:19 -0500

    100/100 2002 S2 Complete

--- Senior/2002/S2_Fraction_Action.py
+++ Senior/2002/S2_Fraction_Action.py
@@ -10,14 +10,21 @@ quotient = num/den
 if whole - quotient == 0:
     print(whole)
 else:
-    fraction = quotient - whole
     new_num = num - (den * whole)
+    # print("new numerator", new_num)
     varying_num = new_num
+    # print(varying_num)
     while varying_num > 1:
-        if (den/varying_num - den//varying_num) == 0:
+        if (den/varying_num - den//varying_num) == 0 and (new_num/varying_num - new_num//varying_num) == 0:
             new_num = new_num/varying_num
             den = den/varying_num
             varying_num = new_num
+            # print("worked")
         else:
             varying_num = varying_num - 1
-    print(whole, str(int(new_num)) + "/" + str(int(den)))
+        # print(varying_num)
+
+    if whole == 0:
+        print(str(int(new_num)) + "/" + str(int(den)))
+    else:
+        print(whole, str(int(new_num)) + "/" + str(int(den)))
